283c021e9be363b5fb854c5bff64fedfa4fdb60a
DiogoSantoss <diogosantoss360@hotmail.com>
2021-04-03 01:26:24 +0100

    Update script to save output

--- Projeto1/script.sh
+++ Projeto1/script.sh
@@ -1,24 +1,29 @@
 FILEBASENAME="dag"
 FOLDERNAME="test"
 INPUTDIR="bigdags"
+FOLDEROUTPUTNAME="outputs"
+OUTPUTDIR="output"
 VERTICES=(100 200 500 1000 2000 5000)
 PROBABILIDADE=(0.1 0.5 0.9)
+
+g++ -std=c++11 -O3 -Wall -o randomDAG randomDAG.cpp -lm
 
 mkdir -p ${FOLDERNAME}
 
 for v in ${VERTICES[@]}; do
     for p in ${PROBABILIDADE[@]}; do
         echo Creating $v $p
-        g++ -std=c++11 -O3 -Wall -o randomDAG randomDAG.cpp -lm
         ./randomDAG $v $p > ${FOLDERNAME}/${FILEBASENAME}$v-$p.txt
     done
 done
 
 g++ -std=c++11 -O3 -Wall -o proj Projeto1.cpp -lm
 
+mkdir -p ${FOLDEROUTPUTNAME}
+
 for FILE in "$FOLDERNAME"/*.txt; do          
         echo "$FILE"                                                                            
-        time ./proj < "$FILE"
+        ./proj < "$FILE" >${FOLDEROUTPUTNAME}/${OUTPUTDIR}_$(basename $FILE).txt
         echo "----------------------"
 done
 
