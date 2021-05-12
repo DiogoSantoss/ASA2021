25b0eb6a1d95be1206b8a0333500315c2bb6be4d
DiogoSantoss <diogosantoss360@hotmail.com>
2021-05-12 21:12:03 +0100

    Updates

--- Projeto1/script.sh
+++ Projeto1/script.sh
@@ -3,7 +3,7 @@ FOLDERNAME="test"
 INPUTDIR="bigdags"
 FOLDEROUTPUTNAME="outputs"
 OUTPUTDIR="output"
-VERTICES=(100 200 500 1000 2000 5000)
+VERTICES=(100 200 500 1000 2000 3000 4000 5000)
 PROBABILIDADE=(0.1 0.5 0.9)
 
 g++ -std=c++11 -O3 -Wall -o randomDAG randomDAG.cpp -lm
@@ -23,8 +23,11 @@ mkdir -p ${FOLDEROUTPUTNAME}
 
 for FILE in "$FOLDERNAME"/*.txt; do          
         echo "$FILE"                                                                            
-        ./proj < "$FILE" >${FOLDEROUTPUTNAME}/${OUTPUTDIR}_$(basename $FILE).txt
+        #./proj < "$FILE" >${FOLDEROUTPUTNAME}/${OUTPUTDIR}_$(basename $FILE).txt
+	time ./proj < "$FILE"
         echo "----------------------"
 done
 
 rm randomDAG && rm proj
+rm -r ${FOLDEROUTPUTNAME}
+rm -r ${FOLDERNAME}
