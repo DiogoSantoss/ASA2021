19d62e871d461b32969b158384658209387e4f28
DiogoSantoss <diogosantoss360@hotmail.com>
2021-05-19 12:23:24 +0100

    Finally 1700

--- Projeto2/Projeto2.cpp
+++ Projeto2/Projeto2.cpp
@@ -11,37 +11,25 @@
 #include <stack>
 #include <algorithm>
 #include <string.h>
-#include <unistd.h>
 
 using namespace std;
 
-/*BFS CONST*/
-#define WHITE -1
-#define GREY 0
-#define BLACK 1
-#define INF 10000
-
-// Each edge has a flux and an id (position in original graph)
 typedef struct{
     int flux;
     int id;
+    int revEdgeIndex;
 } edge;
 
-// Each vertex has an array of edges and necessary params for BFS
-typedef struct{
-    vector<edge> edges;
-    int parentEdge;
-} vertex;
-
 //Residual Graph
-vector<vertex> residual;
-int* parents;
-int* parentEdges;
-int* visited;
-int n;
+vector<vector<edge>> residual;
 
-void printGraph(vector<vertex> g);
-void printStack(stack<int> s);
+//Global Single Variables
+int n, s, t;
+
+//Secondary Arrays
+int* parents;
+int* visited;
+int* parentEdges;
 
 void readInput(){
     /**
@@ -50,80 +38,67 @@ void readInput(){
     int k;
     scanf("%d %d", &n, &k);
 
-    residual = vector<vertex>(n+2);
-    parents = (int*)malloc(sizeof(int)*(n+2));
-    parentEdges = (int*)malloc(sizeof(int)*(n+2));
-    visited = (int*)malloc(sizeof(int)*(n+2));
+    s = 0; t = n+1;
+
+    residual = vector<vector<edge>>(n+1);
+    parentEdges = new int[n+2];
+    parents = new int[n+2];
+    visited = new int[n+1];
 
     int x, y;
     for (int i = 1; i <= n; ++i){
         scanf("%d %d", &x, &y);
+ 
+        edge rX2, rY1;
 
-        edge rX1, rX2, rY1, rY2;
-
-        rX1.flux = x;   rX2.flux = x;
-        rX1.id = 0;     rX2.id = i;
+        rX2.flux = x;   rY1.flux = y;
+        rX2.id = i;     rY1.id = n+1;
         
-        rY1.flux = y;   rY2.flux = y;
-        rY1.id = n+1;   rY2.id = i;
-
-        residual[i].edges.push_back(rX1);   //Aresta do processo i para processador X
-        residual[0].edges.push_back(rX2);   //Aresta do processador X para processo i
-        residual[i].edges.push_back(rY1);   //Aresta do processo i para processador Y
-        residual[n+1].edges.push_back(rY2); //Aresta do processador Y para processo i
+        residual[0].push_back(rX2);   //Aresta do processador X para processo i
+        residual[i].push_back(rY1);   //Aresta do processo i para processador Y
     }
     
     int u, v, c;
     for (int i = 0; i < k; ++i){
         scanf("%d %d %d", &u, &v, &c);
-
+ 
         edge r1, r2;
 
         r1.flux = c; r2.flux = c;
         r1.id = v;   r2.id = u;
+        r1.revEdgeIndex = residual[v].size();
+        r2.revEdgeIndex = residual[u].size();
 
-        residual[u].edges.push_back(r1);  //Aresta do processo u para o processo v
-        residual[v].edges.push_back(r2);  //Aresta do processo v para o processo u
-    }
-}
 
-int findIndex(vector<edge> v, int x){
-    /**
-     * Finds the index of x in the vector v
-     */
-    for (unsigned int i = 0; i < v.size(); ++i){
-        if (v[i].id == x){
-            return i;
-        }
+        residual[u].push_back(r1);  //Aresta do processo u para o processo v
+        residual[v].push_back(r2);  //Aresta do processo v para o processo u
     }
-    return -1;
 }
 
 bool BFS(int s, int t){
     /**
      * Breadth-First Search
      */
-    memset(visited, 0,(n+2)*sizeof(int));
+    memset(visited, 0,(n+1)*sizeof(int));
     memset(parents, -1,(n+2)*sizeof(int));
     memset(parentEdges, -1,(n+2)*sizeof(int));
+
     visited[s] = true;
 
     queue<int> Q;
     Q.push(s);
     
     while(!Q.empty()){
-        int w = Q.front();    
-        Q.pop();
-        for(unsigned int i = 0; i < residual[w].edges.size(); ++i){
-            int id = residual[w].edges[i].id;
-            if(residual[w].edges[i].flux > 0 && !(visited[id])){
+        int w = Q.front(); Q.pop();
+        for(size_t i = 0; i < residual[w].size(); ++i){
+            int id = residual[w][i].id;                           
+            if(residual[w][i].flux > 0 && !(visited[id])){
                 parents[id] = w;
                 parentEdges[id] = i;
-                if (id == t){
-                    return true;                
-                }
-                visited[id] = true;
+                if (id == t)
+                    return true;
                 Q.push(id);
+                visited[id] = true;
             }
         }
     }
@@ -132,33 +107,23 @@ bool BFS(int s, int t){
 
 int EdmondsKarp(){
 
-    stack<int> path = stack<int>();
-    int child, parent, parentEdge, minimum, cur, curEdge, next, nextEdge, total = 0;
-    while(BFS(0,n+1)){
-        child = residual.size()-1;         // Y
-        parent = parents[child];
-        parentEdge = parentEdges[child];  
-        minimum = INF;
-        while (parent >= 0){                  
-            minimum = min(minimum,residual[parent].edges[parentEdge].flux);
-            path.push(child);                 
-            child = parent;
-            parent = parents[child];
-            parentEdge = parentEdges[child];
+    int u, minimum, parentEdge,nextIndex, total = 0;
+    while(BFS(s, t)) {
+        minimum = __INT_MAX__;
+        for (int v = t; v != s; v = parents[v]){
+            parentEdge = parentEdges[v];                          //qual a aresta que leva a v
+            u = parents[v];                                       //qual o pai de v
+            minimum = min(minimum,residual[u][parentEdge].flux);  //aresta de u para v
         }
-
-        path.push(0);
+        for (int v = t; v != s; v = parents[v]){
+            parentEdge = parentEdges[v];
+            u = parents[v];
+            residual[u][parentEdge].flux -= minimum;
+            nextIndex = residual[u][parentEdge].revEdgeIndex;
+            if(u != s && v != t)
+                residual[v][nextIndex].flux += minimum;
+        }
         total += minimum;
-        
-        while(path.size() > 1){
-            cur = path.top();
-            path.pop();
-            next = path.top();
-            nextEdge = parentEdges[next];
-            residual[cur].edges[nextEdge].flux -= minimum;
-            if ((curEdge = findIndex(residual[next].edges, cur)) >= 0)
-                residual[next].edges[curEdge].flux += minimum;
-        }
     }
     return total;
 }
@@ -169,39 +134,6 @@ int EdmondsKarp(){
 
 int main(){
     readInput();
-    //printGraph(residual);
     cout << EdmondsKarp() << endl;
-    free(parents);
-    free(parentEdges);
-    free(visited);
     exit(EXIT_SUCCESS);  
-}
-
-/*************************************************************************************************************************************/
-/*                                                             PRINTS                                                                */
-/*************************************************************************************************************************************/
-void printGraph(vector<vertex> g){
-    /**
-     * Prints graph
-    */
-    cout << "Graph:" << endl;
-    for(unsigned int i = 0; i < g.size(); ++i){
-        cout << i << " _> ";
-        for(unsigned int j = 0; j < g[i].edges.size(); ++j){
-            cout << "(" << g[i].edges[j].id << ", " << g[i].edges[j].flux << ") ";
-        }
-        cout << endl;
-    }
-}
-
-void printStack(stack<int> st){
-    /**
-     * Prints stack
-     */
-    stack<int> s = st;
-    while(!s.empty()){
-        cout << s.top() << ",";
-        s.pop();
-    }
-    cout << endl;
 }
--- Projeto2/script.sh
+++ Projeto2/script.sh
@@ -1,28 +1,37 @@
-FOLDERNAME="inputs"
+FOLDERNAME="gen_inputs"
 FOLDEROUTPUTNAME="our_outputs"
-OUTPUTDIR="output"
+FILEBASENAME="graph"
 
+#Variables for gen2procs
+N=(500 1000 1500 2000)
+C=(50 100 150 200 250 300)
+
+echo "Compiling proj..."
+g++ -std=c++11 -O3 -Wall -o proj2 Projeto2.cpp -lm
+echo "Success"
+echo "Compiling gen2procs..."
+g++ -std=c++11 -O3 -Wall -o gen2procs gen2procs.cpp -lm
+echo "Success"
+
+echo "Creating test files..."
 mkdir -p ${FOLDERNAME}
+for v in ${N[@]}; do
+    for p in ${C[@]}; do
+        echo Creating $v $p
+        ./gen2procs $v $p > ${FOLDERNAME}/${FILEBASENAME}$v-$p.in
+    done
+done
+echo "Done"
 
-g++ -std=c++11 -O3 -Wall -o proj1 tentativa1700.cpp -lm
-g++ -std=c++11 -O3 -Wall -o proj2 Projeto2-1625.cpp -lm
-
+echo "Testing..."
 mkdir -p ${FOLDEROUTPUTNAME}
-
-#o=0
 for FILE in "$FOLDERNAME"/*.in; do          
         echo "$FILE"                                                                            
-        #./proj < "$FILE" >${FOLDEROUTPUTNAME}/${OUTPUTDIR}_$(basename $FILE).out
-        #echo "${FOLDEROUTPUTNAME}/${OUTPUTDIR}_$(basename $FILE).out"
         echo "Lista"
-        time ./proj1 < "$FILE" >${FOLDEROUTPUTNAME}/${OUTPUTDIR}_$(basename $FILE).out
-        echo "Matriz"
         time ./proj2 < "$FILE" >${FOLDEROUTPUTNAME}/${OUTPUTDIR}_$(basename $FILE).out
-        #echo "outputs/output"$o
-        #o=$(($o+1))
         echo "----------------------"
 done
+echo "Done"
 
-rm proj1 && rm proj2
-#rm -r ${FOLDEROUTPUTNAME}
-#rm -r ${FOLDERNAME}
+echo "Removing compiled files..."
+rm proj2 && rm gen2procs
