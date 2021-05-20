587dc04445704368caebaf99d6ecb272d99e9d48
DiogoSantoss <diogosantoss360@hotmail.com>
2021-05-20 01:36:54 +0100

    Improved script

--- Projeto2/script.sh
+++ Projeto2/script.sh
@@ -1,37 +1,55 @@
-FOLDERNAME="gen_inputs"
-FOLDEROUTPUTNAME="our_outputs"
-FILEBASENAME="graph"
+#Variables
+INPUTFOLDER="inputs"
+OUTPUTFOLDER="outputs"
 
 #Variables for gen2procs
 N=(500 1000 1500 2000)
 C=(50 100 150 200 250 300)
 
-echo "Compiling proj..."
-g++ -std=c++11 -O3 -Wall -o proj2 Projeto2.cpp -lm
-echo "Success"
-echo "Compiling gen2procs..."
-g++ -std=c++11 -O3 -Wall -o gen2procs gen2procs.cpp -lm
-echo "Success"
+compileFiles () 
+{
+    echo "Compiling proj..."
+    if ! g++ -std=c++11 -O3 -Wall -o proj2 Projeto2.cpp -lm; then
+        echo "Compilation error!"
+        exit 1
+    fi
+    echo "Success"
 
-echo "Creating test files..."
-mkdir -p ${FOLDERNAME}
+    echo "Compiling gen2procs..."
+    if ! g++ -std=c++11 -O3 -Wall -o gen2procs gen2procs.cpp -lm; then
+        echo "Compilation error!"
+        rm proj2
+    exit 1
+    fi
+    echo "Success"
+}
+
+compileFiles
+
+echo "Test Input:" 
+mkdir -p ${INPUTFOLDER}
+echo "Creating ${INPUTFOLDER}"
 for v in ${N[@]}; do
     for p in ${C[@]}; do
-        echo Creating $v $p
-        ./gen2procs $v $p > ${FOLDERNAME}/${FILEBASENAME}$v-$p.in
+        INPUTFILE=${INPUTFOLDER}/$v-$p.in
+        echo Creating $INPUTFILE
+        ./gen2procs $v $p > $INPUTFILE
     done
 done
 echo "Done"
 
-echo "Testing..."
-mkdir -p ${FOLDEROUTPUTNAME}
-for FILE in "$FOLDERNAME"/*.in; do          
-        echo "$FILE"                                                                            
-        echo "Lista"
-        time ./proj2 < "$FILE" >${FOLDEROUTPUTNAME}/${OUTPUTDIR}_$(basename $FILE).out
-        echo "----------------------"
+echo "Testing:"
+mkdir -p ${OUTPUTFOLDER}
+rm log && touch log
+for FILE in "$INPUTFOLDER"/*.in; do     
+        FILEOUTPUT=${OUTPUTFOLDER}/$(basename $FILE).out      
+        echo $FILE                                                       
+        time ./proj2 < $FILE >$FILEOUTPUT
+        echo $(basename $FILE) >> log
+        cat $FILEOUTPUT >> log
 done
 echo "Done"
 
-echo "Removing compiled files..."
-rm proj2 && rm gen2procs
+echo "Removing compiled and input/output files..."
+rm proj2 && rm gen2procs && rm -r $INPUTFOLDER && rm -r $OUTPUTFOLDER
+echo "Done"
