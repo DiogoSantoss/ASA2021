d3dedf310da17d6fb4d56003f8341234bca3c2b5
DiogoSantoss <diogosantoss360@hotmail.com>
2021-06-24 22:08:44 +0100

    Final clean up

--- Projeto2/script.sh
+++ Projeto2/script.sh
@@ -3,28 +3,11 @@ INPUTFOLDER="inputs"
 OUTPUTFOLDER="outputs"
 
 #Variables for gen2procs
-N=(500 1000 1500 2000)
-C=(50 100 150 200 250 300)
+#N=(500 1000 1500 2000)
+N=(500 600 700 800 900 1000 1100 1200 1300 1400 1500)
+C=(50)
 
-compileFiles () 
-{
-    echo "Compiling proj..."
-    if ! g++ -std=c++11 -O3 -Wall -o proj2 Projeto2.cpp -lm; then
-        echo "Compilation error!"
-        exit 1
-    fi
-    echo "Success"
-
-    echo "Compiling gen2procs..."
-    if ! g++ -std=c++11 -O3 -Wall -o gen2procs gen2procs.cpp -lm; then
-        echo "Compilation error!"
-        rm proj2
-    exit 1
-    fi
-    echo "Success"
-}
-
-compileFiles
+g++ -std=c++11 -O3 -Wall -o gen2procs gen2procs.cpp -lm
 
 echo "Test Input:" 
 mkdir -p ${INPUTFOLDER}
@@ -36,20 +19,4 @@ for v in ${N[@]}; do
         ./gen2procs $v $p > $INPUTFILE
     done
 done
-echo "Done"
-
-echo "Testing:"
-mkdir -p ${OUTPUTFOLDER}
-rm log && touch log
-for FILE in "$INPUTFOLDER"/*.in; do     
-        FILEOUTPUT=${OUTPUTFOLDER}/$(basename $FILE).out      
-        echo $FILE                                                       
-        time ./proj2 < $FILE >$FILEOUTPUT
-        echo $(basename $FILE) >> log
-        cat $FILEOUTPUT >> log
-done
-echo "Done"
-
-echo "Removing compiled and input/output files..."
-rm proj2 && rm gen2procs && rm -r $INPUTFOLDER && rm -r $OUTPUTFOLDER
 echo "Done"
